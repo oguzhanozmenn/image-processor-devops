7914f9400eb651a4fa723f8e97f4fc0d164a48ca
Oğuzhan Özmen <mailin@example.com>
2026-03-24 23:16:46 +0300

    fix: use host.docker.internal for Jenkins to reach LocalStack

--- infra/main.tf
+++ infra/main.tf
@@ -2,19 +2,17 @@ provider "aws" {
   access_key                  = "test"
   secret_key                  = "test"
   region                      = "us-east-1"
+  s3_use_path_style           = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
-  s3_use_path_style           = true
 
   endpoints {
-    s3        = "http://127.0.0.1:4566"
-    s3control = "http://127.0.0.1:4566"
-    dynamodb  = "http://127.0.0.1:4566"
-    sns       = "http://127.0.0.1:4566"
-    iam       = "http://127.0.0.1:4566"
-    lambda    = "http://127.0.0.1:4566"
-    sts       = "http://127.0.0.1:4566"
+    s3       = "http://host.docker.internal:4566"
+    dynamodb = "http://host.docker.internal:4566"
+    lambda   = "http://host.docker.internal:4566"
+    iam      = "http://host.docker.internal:4566"
+    sns      = "http://host.docker.internal:4566"
   }
 }
 
