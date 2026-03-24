fb535c9e6a98c4cfbb098c85a916afadaffcd6c2
Oğuzhan Özmen <mailin@example.com>
2026-03-24 18:30:14 +0300

    fix: recovered resources and updated provider endpoints

--- infra/main.tf
+++ infra/main.tf
@@ -2,30 +2,32 @@ provider "aws" {
   access_key                  = "test"
   secret_key                  = "test"
   region                      = "us-east-1"
-  s3_use_path_style           = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  s3_use_path_style           = true
 
-  # Tüm servisleri LocalStack'e yönlendiriyoruz
   endpoints {
-    s3       = "http://localhost:4566"
-    lambda   = "http://localhost:4566"
-    dynamodb = "http://localhost:4566"
-    sns      = "http://localhost:4566"
-    iam      = "http://localhost:4566"
+    s3        = "http://127.0.0.1:4566"
+    s3control = "http://127.0.0.1:4566"
+    dynamodb  = "http://127.0.0.1:4566"
+    sns       = "http://127.0.0.1:4566"
+    iam       = "http://127.0.0.1:4566"
+    lambda    = "http://127.0.0.1:4566"
+    sts       = "http://127.0.0.1:4566"
   }
 }
 
-# 1. S3 Bucket: Resimlerin yükleneceği yer
+# 1. S3 Bucket
 resource "aws_s3_bucket" "image_upload_bucket" {
   bucket = "user-images-bucket"
 }
 
-# 2. DynamoDB: Resim bilgilerinin tutulacağı tablo
+# 2. DynamoDB Table
 resource "aws_dynamodb_table" "image_metadata" {
-  name           = "ImageMetadata"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "ImageId"
+  name         = "ImageMetadata"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "ImageId"
 
   attribute {
     name = "ImageId"
@@ -33,7 +35,7 @@ resource "aws_dynamodb_table" "image_metadata" {
   }
 }
 
-# 3. SNS Topic: Bildirim kanalı
+# 3. SNS Topic
 resource "aws_sns_topic" "image_proc_topic" {
   name = "image-processing-notifications"
 }
